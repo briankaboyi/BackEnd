413145fb29073dc16a483efc1cc86acbd58465d4
briankaboyi <brianmkaboyi@gmail.com>
2023-11-10 19:58:49 +0300

    House and main minor update

--- First/src/House.java
+++ First/src/House.java
@@ -1,21 +1,21 @@
 public class House {
 
-    int rooms;
-    boolean hasPool;
-    String color;
+  int rooms;
+  boolean hasPool;
+  String color;
 
-    public House(int rooms,boolean hasPool){
-        this.rooms=rooms;
-        this.hasPool=hasPool;
-    }
+  public House(int rooms, boolean hasPool, String red) {
+    this.rooms = rooms;
+    this.hasPool = hasPool;
+  }
 
-    void openDoor(){
-        System.out.println("Door Open all these rooms total"+this.rooms);
-    }
+  void openDoor() {
+    System.out.println("Door Open all these rooms total" + this.rooms);
+  }
 
-    void allSpecs(){
-        System.out.println(rooms + " rooms, has a pool," + hasPool+ ", and is " +color );
-    }
-
-
+  void allSpecs() {
+    System.out.println(
+      rooms + " rooms, has a pool," + hasPool + ", and is " + color
+    );
+  }
 }
